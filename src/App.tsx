f86346fcaabebfc64aabe458df9768c260083a3d
champix56 <desorbaix@free.fr>
2025-02-25 12:30:09 +0100

    limit post message occur

--- src/App.tsx
+++ src/App.tsx
@@ -2,67 +2,77 @@ import { useEffect, useState } from "react";
 //import {decode} from 'querystring'
 import "./App.css";
 import { theme } from "../db.json";
-import CanvasImageLayer from "./CanvasImageLayer.tsx";
-
+import CanvasImageLayer from "./CanvasImageLayer";
+const params = new URL(location.href).searchParams;
+const name = params.get("name");
+const health = params.get("health");
+const attack = params.get("attack");
+const id = params.get("id");
+const images: Array<string> = [];
+const p = params.get("layers");
+const gentype = params.get("type");
 function App() {
   const [base64, setbase64] = useState<string | undefined>(undefined);
-  const [mounted, setmounted] = useState(false);
-  const params = new URL(location.href).searchParams;
-  const name = params.get("name");
-  const health = params.get("health");
-  const attack = params.get("attack");
-  const id = params.get("id");
-  const images: Array<string> = [];
-  const p = params.get("layers");
-  const gentype = params.get("type");
+
   let width = undefined;
   let height = undefined;
-  useEffect(() => {
-    const layers = JSON.parse(null !== p ? p : "");
-    if (!theme.deck.frontUrl.startsWith("http"))
-      theme.deck.frontUrl = theme.startUrl + theme.deck.frontUrl;
-    console.log(theme.deck);
-    theme.parts
-      .filter((e) => {
-        return e.type === "visual";
-      })
-      .forEach((element, index) => {
-        element.assets.forEach((asset: any) => {
-          if (asset.id === layers[index])
-            images.push(theme.startUrl + element.baseUrl + asset.url);
-        });
+  //useEffect(() => {
+  const layers = JSON.parse(null !== p ? p : "");
+  if (!theme.deck.frontUrl.startsWith("http"))
+    theme.deck.frontUrl = theme.startUrl + theme.deck.frontUrl;
+  console.log(theme.deck);
+  theme.parts
+    .filter((e) => {
+      return e.type === "visual";
+    })
+    .forEach((element, index) => {
+      element.assets.forEach((asset: any) => {
+        if (asset.id === layers[index])
+          images.push(theme.startUrl + element.baseUrl + asset.url);
       });
+    });
 
-    if (gentype === "card") {
-      width = theme.deck.width;
-      height = theme.deck.height;
-    } else {
-      width = theme.assetsWidth;
-      height = theme.assetsHeight;
-    }
-    setmounted(true);
-  }, []);
+  if (gentype === "card") {
+    width = theme.deck.width;
+    height = theme.deck.height;
+  } else {
+    width = theme.assetsWidth;
+    height = theme.assetsHeight;
+  }
+  //setmounted(true);
+  //}, []);
   return (
     <div style={{ textAlign: "center" }}>
-      hello
       <div id="img" className="hide">
-        {mounted && (
-          <CanvasImageLayer
-            images={images}
-            type={gentype ? gentype : "card"}
-            //@ts-ignore
-            deck={theme.deck}
-            name={name}
-            attack={parseInt(attack ? attack : "0")}
-            health={parseInt(health ? health : "0")}
-            width={width}
-            height={height}
-            id={id}
-            onDrawn={(b: string) => setbase64(b)}
-          />
-        )}
+        {/* :{mounted && ( */}
+        <CanvasImageLayer
+          images={images}
+          type={gentype ? gentype : "card"}
+          //@ts-ignore
+          deck={theme.deck}
+          name={name}
+          attack={parseInt(attack ? attack : "0")}
+          health={parseInt(health ? health : "0")}
+          width={width}
+          height={height}
+          id={id}
+          onFirstDrawn={(b: string) => {
+            setbase64(b);
+            // @ts-ignore
+            if (window.ReactNativeWebView) {
+              // @ts-ignore
+              window.ReactNativeWebView.postMessage(
+                JSON.stringify({ message: b })
+              );
+            } else console.log("generated");
+          }}
+        />
+        {/* )} */}
       </div>
-      <div id="code">{base64}</div>
+      {
+        //@ts-ignore
+        undefined !== window.ReactNativeWebView && <div id="code">{base64}</div>
+      }
     </div>
   );
 }
--- src/CanvasImageLayer.tsx
+++ src/CanvasImageLayer.tsx
@@ -5,14 +5,16 @@ interface CanvasImageLayerProps {
   images: string[];
   width?: number;
   height?: number;
-  type: "card" | string;
+  type: "card" | "nude";
   deck: IDeck;
   name: string | null;
   health: number | null;
   attack: number | null;
   id: string | null;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  onDrawn: Function;
+  onDrawn?: Function;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  onFirstDrawn?: Function;
 }
 
 const CanvasImageLayer: React.FC<CanvasImageLayerProps> = ({
@@ -24,12 +26,129 @@ const CanvasImageLayer: React.FC<CanvasImageLayerProps> = ({
   attack = 0,
   health = 0,
   onDrawn,
+  onFirstDrawn,
   name,
   id,
 }) => {
+  let isAlreadyDrawn = false;
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const loadDeck = async (ctx: CanvasRenderingContext2D) => {
+    const pr = await new Promise<HTMLImageElement>((resolve, reject) => {
+      if (type !== "card" || undefined === deck) return;
+      const img = new Image();
+      img.crossOrigin = "anonymous"; // Évite les problèmes CORS
+      img.src = deck.frontUrl;
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    });
 
-  useEffect(() => {
+    ctx.drawImage(pr, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    //name
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    let position = deck.positions.find((p) => p.type === "name");
+    let posX = position?.x;
+    //@ts-ignore
+    let posY = position?.y + position?.height;
+    ctx.font = "bold" + position?.height + "px arial";
+    //@ts-ignore
+    ctx.fillText(name, posX, posY);
+    //health
+    position = deck.positions.find((p) => p.type === "health");
+    posX = position?.x;
+    //@ts-ignore
+    posY = position?.y + position?.height;
+    ctx.font = "bold " + position?.height + "px arial";
+    //@ts-ignore
+    ctx.fillText(health.toString(), posX, posY);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    position = deck.positions.find((p) => p.type === "healthbar");
+    //@ts-ignore
+
+    posX = position?.x + (position?.width / 100) * health;
+    //@ts-ignore
+    posY = position?.y + position?.height;
+    //@ts-ignore
+    ctx.arc(posX, posY, position?.height, 0, 2 * Math.PI);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    //attack
+    position = deck.positions.find((p) => p.type === "attack");
+    posX = position?.x;
+    //@ts-ignore
+
+    posY = position?.y + position?.height;
+    ctx.font = "bold " + position?.height + "px arial";
+    //@ts-ignore
+
+    ctx.fillText(attack.toString(), posX, posY);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    position = deck.positions.find((p) => p.type === "attackbar");
+    //@ts-ignore
+    posX = position?.x + (position?.width / 100) * attack;
+    //@ts-ignore
+
+    posY = position?.y + position?.height;
+    //@ts-ignore
+
+    ctx.arc(posX, posY, position?.height, 0, 2 * Math.PI);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "skyblue";
+    position = deck.positions.find((p) => p.type === "id");
+    posX = position?.x;
+    //@ts-ignore
+    posY = position?.y - position?.height;
+    //@ts-ignore
+    ctx.fillText(id, posX, posY);
+    ctx.font = "bold " + position?.height + "px arial";
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  const loadImages = async (ctx: CanvasRenderingContext2D) => {
+    const imageElements = await Promise.all(
+      images.map((src) => {
+        return new Promise<HTMLImageElement>((resolve, reject) => {
+          const img = new Image();
+          img.crossOrigin = "anonymous"; // Évite les problèmes CORS
+          img.src = src;
+          img.onload = () => resolve(img);
+          img.onerror = reject;
+        });
+      })
+    );
+    let imgX = 0;
+    let imgY = 0;
+    let imgW = ctx.canvas.width;
+    let imgH = ctx.canvas.height;
+
+    // Dessiner chaque image chargée sur le canvas
+    imageElements.forEach((img) => {
+      if (type === "card" && undefined !== deck) {
+        const imgposref = deck.positions.find((p) => p.type === "image");
+        //@ts-ignore
+        imgX = imgposref?.x + 50;
+        //@ts-ignore
+        imgY = imgposref?.y + 50;
+        //@ts-ignore
+        imgW = imgposref?.width - 100;
+        //@ts-ignore
+        imgH = imgposref?.height - 100;
+      }
+      ctx.drawImage(img, imgX, imgY, imgW, imgH);
+    });
+  };
+  const drawCanvas = async () => {
     const canvas = canvasRef.current;
     if (!canvas) return;
     const ctx = canvas.getContext("2d");
@@ -37,135 +156,19 @@ const CanvasImageLayer: React.FC<CanvasImageLayerProps> = ({
 
     // Effacer le canvas avant de redessiner
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const loadDeck = async () => {
-      const pr = await new Promise<HTMLImageElement>((resolve, reject) => {
-        if (type !== "card" || undefined === deck) return;
-        const img = new Image();
-        img.crossOrigin = "anonymous"; // Évite les problèmes CORS
-        img.src = deck.frontUrl;
-        img.onload = () => resolve(img);
-        img.onerror = reject;
-      });
 
-      ctx.drawImage(pr, 0, 0, canvas.width, canvas.height);
-      //name
-      ctx.beginPath();
-      ctx.fillStyle = "black";
-      let position = deck.positions.find((p) => p.type === "name");
-      let posX = position?.x;
-      //@ts-ignore
-      let posY = position?.y + position?.height;
-      ctx.font = "bold" + position?.height + "px arial";
-      //@ts-ignore
-      ctx.fillText(name, posX, posY);
-      //health
-      position = deck.positions.find((p) => p.type === "health");
-      posX = position?.x;
-      //@ts-ignore
-      posY = position?.y + position?.height;
-      ctx.font = "bold " + position?.height + "px arial";
-      //@ts-ignore
-      ctx.fillText(health.toString(), posX, posY);
-      ctx.fill();
-      ctx.closePath();
-      ctx.beginPath();
-      position = deck.positions.find((p) => p.type === "healthbar");
-      //@ts-ignore
+    await loadDeck(ctx);
+    await loadImages(ctx);
+    if (!isAlreadyDrawn) {
+      isAlreadyDrawn = true;
+      if (undefined !== onFirstDrawn) onFirstDrawn(canvas.toDataURL());
+    }
 
-      posX = position?.x + (position?.width / 100) * health;
-      //@ts-ignore
-      posY = position?.y + position?.height;
-      //@ts-ignore
-      ctx.arc(posX, posY, position?.height, 0, 2 * Math.PI);
-      ctx.fillStyle = "blue";
-      ctx.fill();
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.fillStyle = "black";
-      //attack
-      position = deck.positions.find((p) => p.type === "attack");
-      posX = position?.x;
-      //@ts-ignore
-
-      posY = position?.y + position?.height;
-      ctx.font = "bold " + position?.height + "px arial";
-      //@ts-ignore
-
-      ctx.fillText(attack.toString(), posX, posY);
-      ctx.fill();
-      ctx.closePath();
-      ctx.beginPath();
-      position = deck.positions.find((p) => p.type === "attackbar");
-      //@ts-ignore
-      posX = position?.x + (position?.width / 100) * attack;
-      //@ts-ignore
-
-      posY = position?.y + position?.height;
-      //@ts-ignore
-
-      ctx.arc(posX, posY, position?.height, 0, 2 * Math.PI);
-      ctx.fillStyle = "blue";
-      ctx.fill();
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.fillStyle = "skyblue";
-      position = deck.positions.find((p) => p.type === "id");
-      posX = position?.x;
-      //@ts-ignore
-      posY = position?.y - position?.height;
-      //@ts-ignore
-      ctx.fillText(id, posX, posY);
-      ctx.font = "bold " + position?.height + "px arial";
-      ctx.fill();
-      ctx.closePath();
-    };
-
-    
-    const loadImages = async () => {
-      const imageElements = await Promise.all(
-        images.map((src) => {
-          return new Promise<HTMLImageElement>((resolve, reject) => {
-            const img = new Image();
-            img.crossOrigin = "anonymous"; // Évite les problèmes CORS
-            img.src = src;
-            img.onload = () => resolve(img);
-            img.onerror = reject;
-          });
-        })
-      );
-      let imgX = 0;
-      let imgY = 0;
-      let imgW = canvas.width;
-      let imgH = canvas.height;
-
-      // Dessiner chaque image chargée sur le canvas
-      imageElements.forEach((img) => {
-        if (type === "card" && undefined !== deck) {
-          const imgposref = deck.positions.find((p) => p.type === "image");
-          //@ts-ignore
-          imgX = imgposref?.x + 50;
-          //@ts-ignore
-          imgY = imgposref?.y + 50;
-          //@ts-ignore
-          imgW = imgposref?.width - 100;
-          //@ts-ignore
-          imgH = imgposref?.height - 100;
-        }
-        ctx.drawImage(img, imgX, imgY, imgW, imgH);
-      });
-      // @ts-ignore
-      if (window.ReactNativeWebView) {
-        // @ts-ignore
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({ message: canvas.toDataURL() })
-        );
-      }
-
-      onDrawn(canvas.toDataURL());
-    };
-    loadDeck();
-    loadImages();
-  }, [images]);
+    if (undefined !== onFirstDrawn) onFirstDrawn(canvas.toDataURL());
+  };
+  useEffect(() => {
+    drawCanvas();
+  }, []);
 
   return <canvas ref={canvasRef} width={width} height={height} />;
 };
